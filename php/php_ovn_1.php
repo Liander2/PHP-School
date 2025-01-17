29e8e796c6e46569ea4d11e93c6e8dee3465bd5e
Liander2 <150666994+Liander2@users.noreply.github.com>
2025-01-17 17:15:30 +0200

    Add files via upload

---
+++ php/php_ovn_1.php
@@ -0,0 +1,15 @@
+<?php
+$myschool = "Prakticum";
+$integer = 4;
+$decimal = "2.5";
+
+?>
+
+<!doctype html>
+<html>
+<body>
+    <p><?php echo $myschool; ?></p>
+    <p><?php echo "$integer * $decimal = " . ($integer * $decimal); ?></p>
+    <p><?php echo '<a href="https://prakticum.fi/">Visit Prakticum</a>'; ?></p>
+</body>
+</html>
